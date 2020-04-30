3226d2f090e9ce550dfdc2b53181b5e2e4787af7
culpinnis <culpinnis@gmail.com>
2020-04-30 20:49:49 +0200

    Added alpha version of k8sticket application

--- cmd/k8sticket/main.go
+++ cmd/k8sticket/main.go
@@ -19,15 +19,16 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"io/ioutil"
-	"os"
-	"strings"
+	"log"
+	"net/http"
 	//"k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/culpinnis/k8sTicket/internal/pkg/k8sfunctions"
+	"github.com/culpinnis/k8sTicket/internal/pkg/proxyfunctions"
+	"github.com/gorilla/mux"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	//
+	"k8s.io/client-go/rest" //
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
 	//
@@ -38,24 +39,13 @@ import (
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
-func Namespace() string {
-	// This way assumes you've set the POD_NAMESPACE environment variable using the downward API.
-	// This check has to be done first for backwards compatibility with the way InClusterConfig was originally set up
-	if ns, ok := os.LookupEnv("POD_NAMESPACE"); ok {
-		return ns
-	}
-
-	// Fall back to the namespace associated with the service account token, if available
-	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
-		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
-			return ns
-		}
-	}
-
-	return "default"
-}
-
 func main() {
+	//http testing
+	r := mux.NewRouter()
+	list := new(proxyfunctions.Serverlist)
+	var prefix = "test"
+	list.Prefix = prefix
+
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -66,28 +56,38 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	myns := Namespace()
+	myns := k8sfunctions.Namespace()
 	fmt.Printf("********\nI am running in %s\n********\n", myns)
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods(myns).List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("\nThere are %d pods in the namespace\n", len(pods.Items))
-		for _,pod := range pods.Items {
-			fmt.Printf("Found Pod %s, IP: %s", pod.GetName(), pod.Status.PodIP)
-			fmt.Print("\nFound Labels\n")
-			for label,value := range pod.GetLabels() {
-				fmt.Printf("########%s : %s\n", label, value)
-			}
-			fmt.Print("\nFound Annotations\n")
-			for annotation,value := range pod.GetAnnotations() {
-				fmt.Printf("########%s : %s\n", annotation, value)
-			}
-			fmt.Print("\n\n")
-		}
-		time.Sleep(10 * time.Second)
+	// get pods in all the namespaces by omitting namespace
+	// Or specify namespace to get pods in particular namespace
+	pods, err := clientset.CoreV1().Pods(myns).List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+	fmt.Printf("\nThere are %d pods in the namespace\n", len(pods.Items))
+	for _, pod := range pods.Items {
+		fmt.Printf("Found Pod %s, IP: %s", pod.GetName(), pod.Status.PodIP)
+		fmt.Print("\nFound Labels\n")
+		for label, value := range pod.GetLabels() {
+			fmt.Printf("########%s : %s\n", label, value)
+		}
+		fmt.Print("\nFound Annotations\n")
+		for annotation, value := range pod.GetAnnotations() {
+			fmt.Printf("########%s : %s\n", annotation, value)
+		}
+		fmt.Print("\n\n")
+		//testing part
+		serverconfig, err := k8sfunctions.PodToConfig(pod)
+		if err == nil {
+			if _, err := list.AddServer(1, serverconfig); err != nil {
+				log.Println("Error Occured: ", err)
+			}
+		}
+	}
+	go list.TicketWatchdog()
+	r.HandleFunc("/"+list.Prefix+"/{s}/{serverpath:.*}", list.MainHandler)
+	r.HandleFunc("/"+list.Prefix, list.ServeHome)
+	r.HandleFunc("/"+list.Prefix+"/", list.ServeHome)
+	r.HandleFunc("/"+list.Prefix+"/ws", list.ServeWs)
+	log.Fatal(http.ListenAndServe(":9001", r))
 }
---
+++ internal/pkg/k8sfunctions/k8sfunctions.go
@@ -0,0 +1,74 @@
+package k8sfunctions
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/culpinnis/k8sTicket/internal/pkg/proxyfunctions"
+	"k8s.io/api/core/v1"
+	//"k8s.io/apimachinery/pkg/api/errors"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+const (
+	default_port = 80
+	default_path = "/"
+)
+
+// Namespace This function returns the namespace of the running pod
+func Namespace() string {
+	// This way assumes you've set the POD_NAMESPACE environment variable using the downward API.
+	// This check has to be done first for backwards compatibility with the way InClusterConfig was originally set up
+	if ns, ok := os.LookupEnv("POD_NAMESPACE"); ok {
+		return ns
+	}
+
+	// Fall back to the namespace associated with the service account token, if available
+	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns
+		}
+	}
+
+	return "default"
+}
+
+// PodToConfig This function reads the k8sTicket annotations and creates
+// a config for the reverse proxy.
+func PodToConfig(pod v1.Pod) (proxyfunctions.Config, error) {
+	ip := pod.Status.PodIP
+	//check if this is a k8sTicket pods
+	if value, ok := pod.GetAnnotations()["ipb-halle.de/k8sTicket"]; ok {
+		if value == "true" {
+			cpath := default_path
+			cport := default_port
+			if path, ok := pod.GetAnnotations()["ipb-halle.de/k8sTicket.path"]; ok {
+				cpath = path
+			}
+			if port, ok := pod.GetAnnotations()["ipb-halle.de/k8sTicket.port"]; ok {
+				_, err := strconv.Atoi(port)
+				if err != nil {
+					log.Println("k8s: Annotation: ", pod.GetName(), ": ", port, ": ", err, " Default ", default_port, "will be used")
+					cport = default_port
+				} else {
+					cport, _ = strconv.Atoi(port)
+				}
+			}
+			return proxyfunctions.Config{Host: ip + ":" + strconv.Itoa(cport), Path: cpath}, nil
+		}
+	}
+	log.Println("k8s: ", pod.GetName(), "is not a k8sTicket pod")
+	return *new(proxyfunctions.Config), errors.New("No a k8sTicket pod")
+}
