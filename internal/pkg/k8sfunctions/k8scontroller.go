a6986c15d846407509aac91453e5d87da1d07a51
culpinnis <culpinnis@gmail.com>
2020-05-11 01:02:56 +0200

    Added code for controller

---
+++ internal/pkg/k8sfunctions/k8scontroller.go
@@ -0,0 +1,59 @@
+package k8sfunctions
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/informers/internalinterfaces"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+)
+
+// Controller object
+type Controller struct {
+	clientset kubernetes.Interface
+	factory   informers.SharedInformerFactory
+	Informer  cache.SharedInformer
+	Stopper   chan struct{}
+}
+
+type Handler struct {
+	AddFunc    func(obj interface{})
+	UpdateFunc func(oldObj, newObj interface{})
+	DeleteFunc func(obj interface{})
+}
+
+func New_proxy_controller(ns string) Controller {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset,
+		0,
+		informers.WithNamespace(ns),
+		informers.WithTweakListOptions(internalinterfaces.TweakListOptionsFunc(func(options *metav1.ListOptions) {
+			options.LabelSelector = "k8sTicket=true"
+		})))
+	informer := factory.Core().V1().Pods().Informer()
+	stopper := make(chan struct{})
+	return (Controller{
+		clientset: clientset,
+		factory:   factory,
+		Informer:  informer,
+		Stopper:   stopper,
+	})
+}
+
+func Add_handler_to_controller(handler *Handler, controller *Controller) {
+	controller.Informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    handler.AddFunc,
+		DeleteFunc: handler.DeleteFunc,
+		UpdateFunc: handler.UpdateFunc,
+	})
+}
