b0f54ae4d18d3dfe8d2c86d9d7494313f14cddc4
culpinnis <culpinnis@gmail.com>
2020-05-15 19:10:37 +0200

    Refinement for Deployment structure

--- cmd/k8sticket/main.go
+++ cmd/k8sticket/main.go
@@ -1,51 +1,15 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	//"k8s.io/apimachinery/pkg/api/errors"
+	"runtime"
 
 	"github.com/culpinnis/k8sTicket/internal/pkg/k8sfunctions"
-	"github.com/culpinnis/k8sTicket/internal/pkg/proxyfunctions"
-	"github.com/gorilla/mux"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest" //
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
-	//http testing
-	r := mux.NewRouter()
-	var prefix = "test"
-	list := proxyfunctions.NewServerlist(prefix)
-
+	namespace := k8sfunctions.Namespace()
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -56,47 +20,9 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	myns := k8sfunctions.Namespace()
-
-	//test controller Functions
-	mycontroller := k8sfunctions.New_proxy_controller(myns)
-	defer close(mycontroller.Stopper)
-	defer runtime.HandleCrash()
-	myhandler := k8sfunctions.New_handler_for_serverlist(list, 1)
-	mycontroller.Informer.AddEventHandler(myhandler)
-	go mycontroller.Informer.Run(mycontroller.Stopper)
-	fmt.Printf("********\nI am running in %s\n********\n", myns)
-	// get pods in all the namespaces by omitting namespace
-	// Or specify namespace to get pods in particular namespace
-	pods, err := clientset.CoreV1().Pods(myns).List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("\nThere are %d pods in the namespace\n", len(pods.Items))
-	/*
-		for _, pod := range pods.Items {
-			fmt.Printf("Found Pod %s, IP: %s", pod.GetName(), pod.Status.PodIP)
-			fmt.Print("\nFound Labels\n")
-			for label, value := range pod.GetLabels() {
-				fmt.Printf("########%s : %s\n", label, value)
-			}
-			fmt.Print("\nFound Annotations\n")
-			for annotation, value := range pod.GetAnnotations() {
-				fmt.Printf("########%s : %s\n", annotation, value)
-			}
-			fmt.Print("\n\n")
-			//testing part
-			serverconfig, err := k8sfunctions.PodToConfig(pod)
-			if err == nil {
-				if _, err := list.AddServer(1, serverconfig); err != nil {
-					log.Println("Error Occured: ", err)
-				}
-			}
-		} */
-	go list.TicketWatchdog()
-	r.HandleFunc("/"+list.Prefix+"/{s}/{serverpath:.*}", list.MainHandler)
-	r.HandleFunc("/"+list.Prefix, list.ServeHome)
-	r.HandleFunc("/"+list.Prefix+"/", list.ServeHome)
-	r.HandleFunc("/"+list.Prefix+"/ws", list.ServeWs)
-	log.Fatal(http.ListenAndServe(":9001", r))
+	proxymap := make(map[string]*k8sfunctions.Proxy_for_deployment)
+	deploymentController := k8sfunctions.New_deployment_controller(namespace)
+	deploymentController.Informer.AddEventHandler(k8sfunctions.New_deployment_handler_for_k8sconfig(clientset, namespace, proxymap))
+	deploymentController.Informer.Run(deploymentController.Stopper)
+	runtime.Goexit()
 }
--- internal/pkg/k8sfunctions/k8scontroller.go
+++ internal/pkg/k8sfunctions/k8scontroller.go
@@ -1,12 +1,17 @@
 package k8sfunctions
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/culpinnis/k8sTicket/internal/pkg/proxyfunctions"
+	gorilla "github.com/gorilla/mux"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/informers/internalinterfaces"
 	"k8s.io/client-go/kubernetes"
@@ -14,19 +19,92 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+type Proxy_for_deployment struct {
+	Pod_controller *Controller
+	Clientset      *kubernetes.Clientset
+	Serverlist     *proxyfunctions.Serverlist
+	Server         *http.Server
+	Router         *gorilla.Router
+	Namespace      string
+	Port           string
+	PodSpec        v1.PodTemplateSpec
+	Stopper        chan struct{}
+}
+
+func New_Proxy_for_deployment(clienset *kubernetes.Clientset, prefix string, ns string, port string, maxTickets int, podspec v1.PodTemplateSpec) *Proxy_for_deployment {
+	proxy := Proxy_for_deployment{}
+	router := gorilla.NewRouter()
+	proxy.Serverlist = proxyfunctions.NewServerlist(prefix)
+	proxy.Namespace = ns
+	proxy.Port = port
+	proxy.Pod_controller = New_pod_controller(clienset, ns)
+	proxy.Pod_controller.Informer.AddEventHandler(New_pod_handler_for_serverlist(proxy.Serverlist, maxTickets))
+	proxy.Clientset = clienset
+	proxy.PodSpec = podspec
+	proxy.Stopper = make(chan struct{})
+	proxy.Router = router
+	proxy.Server = &http.Server{Addr: ":" + port, Handler: router}
+	return &proxy
+}
+
+func (proxy *Proxy_for_deployment) Start() {
+	//defer close(proxy.Pod_controller.Stopper)
+	//defer runtime.HandleCrash()
+	go proxy.Pod_controller.Informer.Run(proxy.Pod_controller.Stopper)
+	go proxy.Serverlist.TicketWatchdog()
+	proxy.Router.HandleFunc("/"+proxy.Serverlist.Prefix+"/{s}/{serverpath:.*}", proxy.Serverlist.MainHandler)
+	proxy.Router.HandleFunc("/"+proxy.Serverlist.Prefix, proxy.Serverlist.ServeHome)
+	proxy.Router.HandleFunc("/"+proxy.Serverlist.Prefix+"/", proxy.Serverlist.ServeHome)
+	proxy.Router.HandleFunc("/"+proxy.Serverlist.Prefix+"/ws", proxy.Serverlist.ServeWs)
+	go log.Fatal(proxy.Server.ListenAndServe())
+}
+
+func (proxy *Proxy_for_deployment) Stop() {
+	close(proxy.Pod_controller.Stopper)
+	runtime.HandleCrash()
+	go func() {
+		if err := proxy.Server.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(proxy.Stopper)
+	}()
+	<-proxy.Stopper
+}
+
 // Controller This struct includes a components of the Controller
 type Controller struct {
-	clientset kubernetes.Interface
+	clientset *kubernetes.Clientset
 	factory   informers.SharedInformerFactory
 	Informer  cache.SharedInformer
 	Stopper   chan struct{}
 }
 
-// New_proxy_controller This function makes a new controller for our proxy
+// New_pod_controller This function makes a new controller for our proxy
 // with a InClusterConfig and a given namespace to watch.
-func New_proxy_controller(ns string) Controller {
+func New_pod_controller(clientset *kubernetes.Clientset, ns string) *Controller {
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset,
+		0,
+		informers.WithNamespace(ns),
+		informers.WithTweakListOptions(internalinterfaces.TweakListOptionsFunc(func(options *metav1.ListOptions) {
+			options.LabelSelector = "k8sTicket=true"
+		})))
+	informer := factory.Core().V1().Pods().Informer()
+	stopper := make(chan struct{})
+	return (&Controller{
+		clientset: clientset,
+		factory:   factory,
+		Informer:  informer,
+		Stopper:   stopper,
+	})
+}
+
+// New_pod_template_controller This function makes a new controller for our proxy
+// with a InClusterConfig and a given namespace to watch.
+func New_deployment_controller(ns string) Controller {
 	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
+	log.Println("New deployment controller started")
+	config, err := rest.InClusterConfig() //I guess it is maybe smarter to give a reference to an exisiting config here instead of generating a new one all the time
 	if err != nil {
 		panic(err.Error())
 	}
@@ -35,13 +113,13 @@ func New_proxy_controller(ns string) Controller {
 	if err != nil {
 		panic(err.Error())
 	}
-	factory := informers.NewSharedInformerFactoryWithOptions(clientset,
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset, //I am not sure if the same factory should be used for all controllers
 		0,
 		informers.WithNamespace(ns),
 		informers.WithTweakListOptions(internalinterfaces.TweakListOptionsFunc(func(options *metav1.ListOptions) {
 			options.LabelSelector = "k8sTicket=true"
 		})))
-	informer := factory.Core().V1().Pods().Informer()
+	informer := factory.Apps().V1().Deployments().Informer()
 	stopper := make(chan struct{})
 	return (Controller{
 		clientset: clientset,
@@ -55,7 +133,7 @@ func New_proxy_controller(ns string) Controller {
 // for a given Servlist. The handler will only handle this specific Serverlist.
 // At the moment there is no use-case for more than one Serverlist, but
 // in the future this could happen.
-func New_handler_for_serverlist(list *proxyfunctions.Serverlist, maxtickets int) cache.ResourceEventHandlerFuncs {
+func New_pod_handler_for_serverlist(list *proxyfunctions.Serverlist, maxtickets int) cache.ResourceEventHandlerFuncs {
 	addfunction := func(obj interface{}) {
 		pod := obj.(*v1.Pod)
 		if pod.Status.Phase == v1.PodRunning {
@@ -81,7 +159,7 @@ func New_handler_for_serverlist(list *proxyfunctions.Serverlist, maxtickets int)
 	}
 	deletefunction := func(obj interface{}) {
 		pod := obj.(*v1.Pod)
-		fmt.Println("k8s: Delete Pod " + pod.Name)
+		log.Println("k8s: Delete Pod " + pod.Name)
 		err := list.SetServerDeletion(pod.Name)
 		if err != nil {
 			log.Println("k8s: SetServerDeletion:  ", err)
@@ -103,7 +181,7 @@ func New_handler_for_serverlist(list *proxyfunctions.Serverlist, maxtickets int)
 			} else if pod_old.Status.Phase == v1.PodRunning && pod_new.Status.Phase != v1.PodRunning {
 				//the pod was changed from PodRunning to sth else
 				deletefunction(pod_new)
-			} else if pod_old.Status.PodIP != pod_new.Status.PodIP {
+			} else if pod_old.Status.PodIP != pod_new.Status.PodIP { //I guess this should not happen
 				deletefunction(pod_old)
 				addfunction(pod_new)
 			} else {
@@ -124,4 +202,65 @@ func New_handler_for_serverlist(list *proxyfunctions.Serverlist, maxtickets int)
 			}
 		},
 	})
+}
+
+func New_deployment_handler_for_k8sconfig(clientset *kubernetes.Clientset, ns string, proxies map[string]*Proxy_for_deployment) cache.ResourceEventHandlerFuncs {
+	addfunction := func(obj interface{}) {
+		deployment := obj.(*appsv1.Deployment)
+		log.Println("k8s: Adding deployment" + deployment.Name)
+		if _, ok := proxies[deployment.Name]; !ok {
+			var port string
+			if _, ok := deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.port"]; !ok {
+				port = "8080"
+			} else {
+				port = deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.port"]
+			}
+			var prefix string
+			if _, ok := deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.prefix"]; !ok {
+				prefix = deployment.Name
+			} else {
+				prefix = deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.prefix"]
+			}
+			var maxTickets int
+			if _, ok := deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.maxTickets"]; !ok {
+				maxTickets = 1
+			} else {
+				_, err := strconv.Atoi(deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.maxTickets"])
+				if err != nil {
+					log.Println("k8s: Deployment: " + deployment.Name + "ipb-halle.de/k8sticket.deployment.maxTickets annotation malformed: " + err.Error())
+					maxTickets = 1
+				} else {
+					maxTickets, _ = strconv.Atoi(deployment.GetAnnotations()["ipb-halle.de/k8sticket.deployment.maxTickets"])
+				}
+			}
+			proxies[deployment.Name] = New_Proxy_for_deployment(clientset, prefix, ns, port, maxTickets, deployment.Spec.Template)
+			go proxies[deployment.Name].Start()
+		} else {
+			log.Println("k8s: New_deployment_handler_for_k8sconfig: Deployment " + deployment.Name + " already exists!")
+		}
+		/*podtmp := deployment.Spec.Template
+		mypod := v1.Pod{
+			ObjectMeta: podtmp.ObjectMeta,
+			Spec:       podtmp.Spec,
+		}
+		mypod.ObjectMeta.Labels["k8sautogen"] = "true"
+		mypod.GenerateName = "testpod"
+		_, err := clientset.CoreV1().Pods(ns).Create(&mypod)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Pod created successfully...")*/
+	}
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc: addfunction,
+		DeleteFunc: func(obj interface{}) {
+			deployment := obj.(*appsv1.Deployment)
+			log.Println("k8s: Deleting deployment" + deployment.Name)
+			if _, ok := proxies[deployment.Name]; !ok {
+				log.Println("k8s: New_deployment_handler_for_k8sconfig: Deployment " + deployment.Name + " is not known!")
+			} else {
+				proxies[deployment.Name].Stop()
+			}
+		},
+	}
 }
